8f46c152ff118c7cf578f39fef4cc201a6f84ab2
Karan Rohra <64662748+KaranRohra@users.noreply.github.com>
2024-06-17 10:20:45 +0530

    [#6]: Trade All Symbols of Basket (#7)
    * Added multiple symbol trading feature
    
    * Updated details in trade start and README.md
    
    * Added runtime file for heroku
    
    * Added basket refresh in 2minutes interval feature

--- trading.py
+++ trading.py
@@ -9,43 +9,67 @@ from mail import app as ma
 from db import MongoDB
 
 
-def trading_start_notification(exchange, symbol):
-    trade_details = {
-        "exchange": exchange,
-        "symbol": symbol,
-        "entry_time_frame": os.environ[Env.ENTRY_TIME_FRAME] + "minutes",
-        "exit_time_frame": os.environ[Env.EXIT_TIME_FRAME] + "minutes",
-    }
-    ma.send_trading_started_email(**trade_details)
+def trading_start_notification(basket_items):
+    trade_details_lst = [
+        {
+            "heading": "Time Frame",
+            "key_value": {
+                "entry_time_frame": os.environ[Env.ENTRY_TIME_FRAME] + "minutes",
+                "exit_time_frame": os.environ[Env.EXIT_TIME_FRAME] + "minutes",
+            },
+        }
+    ]
+
+    for sd in basket_items:
+        trade_details_lst.append(
+            {
+                "heading": "Symbol Details" if len(trade_details_lst) == 1 else "",
+                "key_value": {
+                    "exchange": sd["exchange"],
+                    "symbol": sd["tradingsymbol"],
+                },
+            }
+        )
+
+    ma.send_trading_started_email(trade_details_lst)
     MongoDB.insert_log(
-        log_type=LogType.INFO, message="Trading started"
+        log_type=LogType.INFO, message="Trading started", details=[d["key_value"] for d in trade_details_lst]
     )
 
 
 def start():
-    symbol_details = kite_utils.get_basket_item()
-    exchange, symbol = symbol_details["exchange"], symbol_details["tradingsymbol"]
-
-    trading_start_notification(exchange, symbol)
     entry_time_frame = int(os.environ[Env.ENTRY_TIME_FRAME])
     exit_time_frame = int(os.environ[Env.EXIT_TIME_FRAME])
 
+    basket_items = kite_utils.get_basket_items()
+    trading_start_notification(basket_items)
+
     while mu.is_market_open()["is_market_open"]:
         now = dt.now()
-        if now.minute % entry_time_frame == 0 and now.second == 0:
-            holding = kite_utils.get_holding_by_symbol(exchange, symbol)
-            if not holding:
-                orders.search_entry(symbol_details)
-        if now.minute % exit_time_frame == 0 and now.second == 0:
-            holding = kite_utils.get_holding_by_symbol(exchange, symbol)
-            if holding:
-                orders.search_exit(holding)
+        if now.minute % 2 == 0 and now.second == 1:
+            basket_items_updated = kite_utils.get_basket_items()
+            if str(basket_items) != str(basket_items_updated):
+                basket_items = basket_items_updated
+                trading_start_notification(basket_items)
+
+        for sd in basket_items:  # SD = Symbol Details
+            exchange, symbol = sd["exchange"], sd["tradingsymbol"]
+            if now.minute % entry_time_frame == 0 and now.second == 0:
+                holding = kite_utils.get_holding_by_symbol(exchange, symbol)
+                if not holding:
+                    orders.search_entry(sd)
+            if now.minute % exit_time_frame == 0 and now.second == 0:
+                holding = kite_utils.get_holding_by_symbol(exchange, symbol)
+                if holding:
+                    orders.search_exit(holding)
         time.sleep(1)
 
     end_time = mu.is_market_open()["end_time"] + timedelta(minutes=1)
     if end_time > dt.now():
-        holding = kite_utils.get_holding_by_symbol(exchange, symbol)
-        if holding:
-            orders.search_exit(holding)
-        else:
-            orders.search_entry(symbol_details)
+        for sd in basket_items:  # SD = Symbol Details
+            exchange, symbol = sd["exchange"], sd["tradingsymbol"]
+            holding = kite_utils.get_holding_by_symbol(exchange, symbol)
+            if holding:
+                orders.search_exit(holding)
+            else:
+                orders.search_entry(sd)
