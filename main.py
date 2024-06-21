a1bbb7ed3e7dedd7d8e618eee650dc10abd0b82a
Karan Rohra <rohrakaran38@gmail.com>
2024-06-22 02:43:22 +0530

    Added numpy dependency

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import warnings
-
-warnings.filterwarnings("ignore", message="numpy.dtype size changed")
-warnings.filterwarnings("ignore", message="numpy.ufunc size changed")
-
 import dotenv
 
 dotenv.load_dotenv()
